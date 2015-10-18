aefba1459001a1dab31ce4f7ed82cd50fc0df248
Jingwei Yang <yangjingwei@ws312tk.int.tc.columbia.edu>
2015-10-18 18:12:41 -0400

    add primary key to studentID, get student by ID, list all students and add student were supported

--- ssis/students/models.py
+++ ssis/students/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 class Student(models.Model):
-	studentID = models.CharField(max_length=100, blank=True, default='');
+	studentID = models.CharField(primary_key = True, max_length=100, blank=False, default='');
 	lastName = models.CharField(max_length=100, blank=True, default='');
 	firstName = models.CharField(max_length=100, blank=True, default='');
 
--- ssis/students/urls.py
+++ ssis/students/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from students import views
 
 urlpatterns = [
-    url(r'^students/$', views.student_list),
+    url(r'^api/students/$', views.student_list),
     #url(r'^', views.student_list),
-    url(r'^students/(?P<sID>[0-9]+)/$', views.student_detail),
+    url(r'^api/students/(?P<sID>[A-Za-z]{2}[\d]{4})/$', views.student_detail), 
 ]
