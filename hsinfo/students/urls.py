15cce620e84e345c7bda08c5cf8a302884c6e73b
Jingwei Yang <yangjingwei@dyn-160-39-234-159.dyn.columbia.edu>
2015-12-12 00:04:06 -0500

    K-12 microservise alwasy done, missing secondary index

--- hsinfo/students/urls.py
+++ hsinfo/students/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from students import views
 
+
 urlpatterns = [
-    url(r'^$', views.student_list),
-    url(r'^(?P<ssn>[\d]{9})', views.student_detail),
+    url(r'^api/k12/$', views.student_list),
+    url(r'^api/k12/(?P<ssn>[0-9]{9})/$', views.student_detail),
 ]
--- hsinfo/students/views.py
+++ hsinfo/students/views.py
@@ -3,10 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-import students.get_item as db_get
-import students.add_item as db_add
-import students.update_item as db_update
-import students.delete_item as db_delete
+from students.lib import dbop as dbop
+
+
 
 class JsonResponse(HttpResponse):
     def __init__(self, data, **kwargs):
@@ -14,34 +13,41 @@ class JsonResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JsonResponse, self).__init__(content, **kwargs)
 
-# Create your views here.
 
+
+# Create your views here.
 @csrf_exempt
 def student_list(request):
     if request.method == 'GET':
-        result = db_get.get_all()
+        result = dbop.get_all()
         if 'Items' not in result:
             return HttpResponse(status=204)
         return JsonResponse(result['Items'])
     if request.method == 'POST':
+        print request
         data = JSONParser().parse(request)
-        db_add.load_from_json_dict(data)
+        print data
+        dbop.load_from_json_dict(data)
+        dbop.add(data)
         return JsonResponse(data)
+
+
 
 @csrf_exempt
 def student_detail(request, ssn):
     if request.method == 'GET':
         student = {'SSN': ssn}
-        result = db_get.get(student)
+        result = dbop.get(student)
         if 'Item' not in result:
             return HttpResponse(status=204)
         return JsonResponse(result['Item'])
     if request.method == 'PUT':
         student = {'SSN': ssn}
         data = JSONParser().parse(request)
-        result = db_update.update(student, data)
+        dbop.delete(student)
+        dbop.add(data)
         return JsonResponse(data)
     if request.method == 'DELETE':
         student = {'SSN': ssn}
-        result = db_delete.delete(student)
+        result = dbop.delete(student)
         return HttpResponse()
